d3f9bda62210e4a26ab84c9078e56abe0264b547
nelhendawi <42819792+nelhendawi@users.noreply.github.com>
2019-03-28 23:28:11 -0600

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,6 +1,5 @@
 #ask the TA how to save bar plot 
 #ask the TA why months start at 0 
-#ask the TA about data not entered
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,43 +20,46 @@ def connect(path):
 
 def task1():
     global connection, cursor
-    #connection.row_factory ==sqlite3.Row
-    #cursor = connection.cursor()
-    startyear = int(input("Enter start year (YYYY): "))
-    endyear = int(input("Enter end year (YYYY): "))
-    crimetype = input("Enter crime type: ")
-    #df=pd.read_sql_query("SELECT SUM(Incidents_Count), Month FROM crime_incidents WHERE Crime_Type=:crimetype AND YEAR>=:startyear AND YEAR <=:endyear GROUP BY Month;",{"crimetype":crimetype,"startyear":startyear,"endyear":endyear},connection)
+    #take input of start_year,end_year and crime_type
+    startyear=int(input("Enter start year (YYYY):"))
+    endyear=int(input("Enter end year (YYYY):"))
+    crimetype=input("Enter crime type:")
+    #SQL STATAMENT TO GET THE HOW MANY TIMES A CRTIME HAPPENS IN THE RANGE GIVEN
     cursor.execute("SELECT SUM(Incidents_Count), Month FROM crime_incidents WHERE Crime_Type=:crimetype AND YEAR>=:startyear AND YEAR <=:endyear GROUP BY Month;",{"crimetype":crimetype,"startyear":startyear,"endyear":endyear})
+    #Plot the graph and save it in the same folder as the assignment.
     df = pd.DataFrame(cursor.fetchall())  
-    plot = df.plot.bar(x = "Month")
+    print(df)
+    plot=df.plot.bar(x=1)
     plot.set_xlabel("Month")
     plt.plot()
     plt.show()  
-
-
+    
 def task2(count):
     global connection, cursor
+    #asks the user for a number of locations
     N = int(input("Enter number of locations: "))
+    #runs a query to find the 3 most populous locations in edmonton returning their name, population, and coordinates
     cursor.execute("SELECT p.Neighbourhood_Name, (p.CANADIAN_CITIZEN + p.NON_CANADIAN_CITIZEN + p.NO_RESPONSE) AS 'Tot' , c.Latitude, c.Longitude FROM population p, coordinates c WHERE p.Neighbourhood_Name = c.Neighbourhood_Name AND Tot != 0 AND c.Latitude != 0 ORDER BY Tot DESC LIMIT :number;", {"number": N}) 
     top = cursor.fetchall() 
+    #runs a query to find the 3 least populous location in edmonton returning their name, population, and coordinates
     cursor.execute("SELECT p.Neighbourhood_Name, (p.CANADIAN_CITIZEN + p.NON_CANADIAN_CITIZEN + p.NO_RESPONSE) AS 'Tott' , c.Latitude, c.Longitude FROM population p, coordinates c WHERE p.Neighbourhood_Name = c.Neighbourhood_Name AND Tott != 0 AND c.Latitude != 0 ORDER BY Tott LIMIT :number;", {"number": N}) 
     bottom = cursor.fetchall()
-
-    m = folium.Map(location=[53.532407, -113.493805], zoom_start=12)
-
+    #instantiates the map of edmonton
+    m = folium.Map(location=[53.5444,-113.323], zoom_start=11)
+    #creates markers for the top 3 populations in edmonton
     for spot in top:
         folium.Circle(location=[spot[2],spot[3]], popup= spot[0] +"<br>"+ str(spot[1]), radius= spot[1]/7, color= 'crimson', fill= True, fill_color= "crimson").add_to(m)
-   
+    #creates markers for the bottom 3 populations in edmonton
     for spot in bottom:
         folium.Circle(location=[spot[2],spot[3]], popup= spot[0] +"<br>"+ str(spot[1]), radius= spot[1], color= 'crimson', fill= True, fill_color= "crimson").add_to(m)
-   
+    #saves the changes made to the origional map  
     m.save("Q2-"+str(count)+".html")
 
 def task3(count):
     global connection, cursor
     #asks the user for a year range, type of crime, and number of locations
-    start_year = int(input("Enter start year (YYYY): "))
-    end_year = int(input("Enter end year (YYYY): "))
+    start_year = int(input("Start year: "))
+    end_year = int(input("End year: "))
     crime = input("Enter a crime type: ")
     N = int(input("Enter number of locations: "))
     #runs a query to find the top N locations where the specific crime in the year range is highest
