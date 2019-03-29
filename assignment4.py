14ca9eef1aaa7b23130c2389e87ca8b4d9eff325
Nicolas Carpenter <42122055+NicoCarpe@users.noreply.github.com>
2019-03-28 20:23:15 -0600

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
+import folium
 connection = None
 cursor = None
 
@@ -13,25 +14,67 @@ def connect(path):
     cursor.execute(' PRAGMA foreign_keys=ON; ')
     connection.commit()
     return
+
 def task1():
     global connection, cursor
     startyear=int(input("Enter start year (YYYY):"))
     endyear=int(input("Enter end year (YYYY):"))
     crimetype=input("Enter crime type:")
-    df=pd.read_sql_query('SELECT SUM(Incidents_Count) FROM crime_incidents WHERE Crime_Type=crimetype AND YEAR>=startyear AND YEAR <=endyear GROUP BY Month;',{"crimetype":crimetype,"startyear":startyear,"endyear":endyear})
+    df=pd.read_sql_query('SELECT SUM(Incidents_Count), Month FROM crime_incidents WHERE Crime_Type=crimetype AND YEAR>=startyear AND YEAR <=endyear GROUP BY Month;',{"crimetype":crimetype,"startyear":startyear,"endyear":endyear}, connection)
     plot=df.plot.bar(x='Month')
     plt.plot()
     plt.show()
     input('Please press enter to continue')
+
+def task2():
+    global connection, cursor
+    N = int(input("Enter number of locations: "))
+    cursor.execute("SELECT p.Neighbourhood_Name, (p.CANADIAN_CITIZEN + p.NON_CANADIAN_CITIZEN + p.NO_RESPONSE) AS 'Tot' , c.Latitude, c.Longitude FROM population p, coordinates c WHERE p.Neighbourhood_Name = c.Neighbourhood_Name AND Tot != 0 AND c.Latitude != 0 ORDER BY Tot DESC LIMIT :number;", {"number": N}) 
+    top = cursor.fetchall() 
+    cursor.execute("SELECT p.Neighbourhood_Name, (p.CANADIAN_CITIZEN + p.NON_CANADIAN_CITIZEN + p.NO_RESPONSE) AS 'Tott' , c.Latitude, c.Longitude FROM population p, coordinates c WHERE p.Neighbourhood_Name = c.Neighbourhood_Name AND Tott != 0 AND c.Latitude != 0 ORDER BY Tott LIMIT :number;", {"number": N}) 
+    bottom = cursor.fetchall()
+
+    m = folium.Map(location=[53.5444,-113.323], zoom_start=11)
+
+    for spot in top:
+        folium.Circle(location=[spot[2],spot[3]], popup= spot[0] +"<br>"+ str(spot[1]), radius= spot[1]/7, color= 'crimson', fill= True, fill_color= "crimson").add_to(m)
+   
+    for spot in bottom:
+        folium.Circle(location=[spot[2],spot[3]], popup= spot[0] +"<br>"+ str(spot[1]), radius= spot[1], color= 'crimson', fill= True, fill_color= "crimson").add_to(m)
+   
+    m.save("sample_marker.html")
+
 def main():
     #creates the database
-    path = "a4.db"
+    path = "./a4-sampled.db"
     connect(path)
     #bool value to exit program
     endGame = False    
     #all the tasks explanations
     tasklist=['1: Q1','2: Q2','3: Q3','4: Q4','E: Exit']
-    task1()
+    while not endGame:
+        for i in tasklist:
+            print(i)
+        select=input("Enter your choice: ")
+        if (select=='1'):
+            task1()
+            print('')
+        elif(select=='2'):
+            task2()
+            print('')
+        elif(select=='3'):
+            task3()
+            print('')
+        elif(select=='4'):
+            task4()
+            print('')
+        elif(select=='Exit'):
+            endGame=True
+        else:
+            print('')
+            print('Please enter a correct command.')
+            print('')                
+    
     connection.commit()
     connection.close()
     return    
